98f3c67a22c538bbe7d44f1fc9825f927eb4b991
Scott Winner <scottbwinner@gmail.com>
2026-08-01 01:45:35 -0500

    bare boned Dataset class

---
+++ src/dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+class Zinc250kDataset(Dataset):
+    def __init__(self, encoded_smiles):
+        """
+        Args:
+            encoded_smiles: encoded and padded SMILES sequences as tensor of shape (N, max_len) dtype torch.long
+        """
+        self.encoded_smiles = encoded_smiles
+
+    def __len__(self):
+        return len(self.encoded_smiles)
+
+    def __getitem__(self, idx):
+        return self.encoded_smiles[idx]
